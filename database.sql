9df66959d9b894ff554c457b6a4a77f0b64faf9c
Mark Spannbauer <msmarkieer@gmail.com>
2020-03-26 22:08:48 -0500

    way more styling than I should have at this stage

--- database.sql
+++ database.sql
@@ -3,6 +3,16 @@ CREATE TABLE todolist
     id SERIAL PRIMARY KEY,
     name VARCHAR (120) NOT NULL,
     importance INTEGER NOT NULL,
-    notes text DEFAULT 'Want to Read',
+    notes text,
     completed text DEFAULT 'NO'
 );
+
+INSERT INTO todolist
+    (name, importance, notes)
+VALUES('Do the dishes', 4, '(passive agressively)');
+INSERT INTO todolist
+    (name, importance, notes)
+VALUES('Laundry', 3, 'Don''t do roommates');
+INSERT INTO todolist
+    (name, importance, notes, completed)
+VALUES('Eat leftovers', 1, 'roomates not your own', 'YES'); 
