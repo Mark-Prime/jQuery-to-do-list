fc4fd000980e4be52f377462c34f80fd27e70207
Mark Spannbauer <msmarkieer@gmail.com>
2020-03-30 17:09:45 -0500

    Fixed a typo

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/list.routes')
 
-var port = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
